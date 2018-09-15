d0ce874a768b97cd8730e333512fe39ecfbab2ba
Max Gushlenko <m.gushlenko@appyfurious.com>
2018-09-15 16:27:14 +0300

    firt push

---
+++ RealmPractice/Models/Employee.swift
@@ -0,0 +1,13 @@
+//
+//  Employee.swift
+//  RealmPractice
+//
+//  Created by Max Gushlenko on 9/15/18.
+//  Copyright © 2018 MG. All rights reserved.
+//
+
+import UIKit
+
+class Employee: NSObject {
+
+}
